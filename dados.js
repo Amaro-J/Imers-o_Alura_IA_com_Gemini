c4b3728bc2da2d22975a18dfde7fd0468080104a
Amaro-J <jairodemelo70@gmail.com>
2024-09-07 00:22:10 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,120 @@
+let dados = [
+    {
+titulo: "Machado de Assis",
+descricao: "Machado de Assis, um dos maiores nomes da literatura brasileira, era conhecido por sua habilidade em desvendar a complexidade da alma humana e a ironia com que analisava a sociedade. Seus romances, como 'Dom Casmurro' e 'Memórias Póstumas de Brás Cubas', revolucionaram a literatura nacional ao introduzir o Realismo, com personagens complexos e narrativas que questionavam as verdades absolutas. Além da prosa, Machado também se destacou como poeta e cronista, deixando um legado riquíssimo que continua a influenciar escritores e leitores até os dias de hoje. Sua obra, marcada pela profundidade psicológica e pela linguagem refinada, o consagrou como um dos maiores escritores da língua portuguesa.",
+link: "https://pt.wikipedia.org/wiki/Machado_de_Assis",
+tags: "Machado de Assis Literatura Brasileira Realismo Dom Casmurro Memórias Póstumas de Brás Cubas Psicologia na Literatura Ironia Literatura Clássica Escritores Brasileiros"
+},
+
+{
+titulo : "Aluísio Azevedo",
+descricao : "Aluísio Azevedo foi um dos principais representantes do Naturalismo no Brasil. Suas obras, como 'O Cortiço' e 'O Mulato', retratam a realidade social de forma crua e realista, com foco nas classes menos favorecidas e nos problemas urbanos. Seus romances, marcados pela linguagem coloquial e pela descrição detalhada dos ambientes, contribuíram para a renovação da literatura brasileira.",
+link : "https://pt.wikipedia.org/wiki/Alu%C3%ADsio_Azevedo",
+tags: "Aluísio Azevedo Naturalismo Literatura Brasileira O Cortiço O Mulato Realismo Social Classes Sociais Problemas Urbanos Literatura Brasileira do Século XIX"
+},
+
+{
+titulo : "Castro Alves",
+descricao : "Castro Alves, o 'Poeta dos Escravos', foi um dos maiores nomes da poesia romântica brasileira. Seus versos apaixonados e combativos denunciavam as injustiças sociais e a escravidão, tornando-o um ícone da luta abolicionista. Obras como 'O Navio Negreiro' e 'Vozes d'África' marcaram a literatura brasileira e continuam a emocionar os leitores.",
+link : "https://pt.wikipedia.org/wiki/Castro_Alves",
+tags: "Castro Alves Poesia Romântica Literatura Brasileira Abolicionismo O Navio Negreiro Vozes d'África Poesia Social Poetas Brasileiros"
+},
+
+{
+titulo: "João do Rio",
+descricao: "João do Rio foi um cronista genial que soube capturar a essência da cidade do Rio de Janeiro. Sua obra é um tesouro para a literatura brasileira, oferecendo um rico retrato da vida urbana e da sociedade da época.",
+link: "https://pt.wikipedia.org/wiki/Jo%C3%A3o_do_Rio",
+tags: "João do Rio Crônica Literatura Brasileira Rio de Janeiro Jornalismo Literário Vida Urbana Cultura Carioca"
+},
+
+{
+titulo: "Gregório de Matos",
+descricao: "Poeta Barroco, conhecido como 'Boca do Inferno', Gregório de Matos foi um dos maiores poetas barrocos brasileiros. Sua obra é marcada pela ironia, pelo erotismo e pela crítica social.Seu estilo poético abrangia desde a sátira e a poesia religiosa até a lírica amorosa. Seus poemas abordavam temas como a vida urbana, a corrupção, a hipocrisia e a fugacidade da vida",
+link: "https://pt.wikipedia.org/wiki/Gregório_de_Matos",
+tags: "Gregório de Matos Barroco Poesia Brasileira Boca do Inferno Sátira Erotismo Poesia Religiosa Literatura Brasileira Crítica Social"
+},
+
+{
+titulo: "Antônio Gonçalves Dias",
+descricao: "Antônio Gonçalves Dias foi um dos maiores poetas românticos brasileiros. Seus poemas exaltavam a natureza, a pátria e o índio, contribuindo para a construção de um imaginário nacional",
+link: "https://pt.wikipedia.org/wiki/Ant%C3%B3nio_Gon%C3%A7alves_Dias",
+tags: "Antônio Gonçalves Dias Romantismo Poesia Brasileira Nacionalismo Índio Natureza Poesia Épica Literatura Brasileira"
+},
+
+{
+titulo: "Álvares de Azevedo",
+descricao: "Álvares de Azevedo foi um dos principais representantes da segunda geração romântica brasileira. Sua poesia é marcada pelo pessimismo, pelo individualismo e pela valorização da morte.",
+link: "https://pt.wikipedia.org/wiki/%C3%81lvares_de_Azevedo",
+tags: "Álvares de Azevedo Ultrarromantismo Poesia Brasileira Mal do Século Pessimismo Individualismo Culto à Morte"
+},
+
+{
+titulo: "Afonso Henriques de Lima Barreto",
+descricao: "Lima Barreto foi um romancista, contista e cronista brasileiro. Sua obra, marcada por um forte realismo crítico, denuncia as desigualdades sociais e as mazelas da sociedade brasileira.",
+Link: "https://pt.wikipedia.org/wiki/Lima_Barreto",
+tags: "Lima Barreto Realismo Crítico Literatura Brasileira Romancista Brasileiro Desigualdade Social Crítica Social"
+},
+
+{
+titulo: "Alberto Caeiro da Silva",
+descricao: "Alberto Caeiro foi um heterônimo criado pelo poeta Fernando Pessoa. Representando a voz da natureza e da simplicidade, Caeiro é conhecido por seus poemas que celebram a vida simples e a conexão com o mundo natural.",
+link: "https://pt.wikipedia.org/wiki/Alberto_Caeiro",
+tags: "Alberto Caeiro Fernando Pessoa Heterônimo Poesia Natureza Simplicidade Existencialismo Poesia Moderna"
+},
+
+{
+titulo: "Adolfo Ferreira dos Santos Caminha",
+descricao: "Adolfo Caminha foi um escritor naturalista brasileiro, conhecido por sua obra 'Bom-Crioulo'. O romance, considerado um marco do Naturalismo no Brasil, aborda temas como a homossexualidade e a marginalidade.",
+link: "https://pt.wikipedia.org/wiki/Adolfo_Caminha",
+tags: "Adolfo Caminha Naturalismo Literatura Brasileira Bom-Crioulo Homossexualidade Marginalidade Realismo"
+},
+
+{
+titulo: "Alexandre Herculano",
+descricao: "Alexandre Herculano foi um historiador e romancista português, mas sua influência na literatura brasileira foi significativa. Seus romances históricos abordam temas como a formação nacional e a identidade portuguesa.",
+link: "https://pt.wikipedia.org/wiki/Alexandre_Herculano",
+tags: "Alexandre Herculano História Romance Histórico Literatura Portuguesa Formação Nacional Identidade Cultural"
+},
+   
+{
+titulo: "Artur Nabantino Gonçalves de Azevedo",
+descricao: "Artur Azevedo foi um dramaturgo, romancista e jornalista brasileiro. Seus trabalhos, marcados pelo humor e pela crítica social, retratam a vida carioca do final do século XIX e início do século XX.",
+Link: "https://pt.wikipedia.org/wiki/Artur_Azevedo_(escritor)",
+tags: "Artur Azevedo Drama Romance Literatura Brasileira Rio de Janeiro Humor Crítica Social"
+},
+
+{
+titulo: "Artur Azevedo",
+descricao: "Artur Azevedo, também conhecido como Artur da Silva, foi um escritor português que se destacou na poesia e no teatro. Sua obra, marcada pelo Romantismo, influenciou a literatura brasileira.",
+link: "https://pt.wikipedia.org/wiki/Artur_Azevedo_(poeta)",
+tags: "Artur Azevedo Poesia Romantismo Literatura Portuguesa Influência Literária Amor Natureza Melancolia"
+},
+   
+{
+titulo: "Auta de Sousa",
+descricao: "Auta de Sousa foi uma escritora brasileira, pioneira do feminismo no país. Seus romances e contos abordam temas como a condição feminina, o casamento e a busca por independência.",
+link: "https://pt.wikipedia.org/wiki/Auta_de_Sousa",
+tags: "Auta de Sousa Feminismo Literatura Brasileira Contos Romances Condição Feminina Independência Feminina"
+},
+
+{
+titulo: "Bernardo Guimarães",
+descricao: "Bernardo Guimarães foi um romancista brasileiro, conhecido por seus romances regionalistas que retratam a vida no interior do Brasil. Sua obra mais famosa é 'O Guarani', um romance indianista.",
+link: "https://pt.wikipedia.org/wiki/Bernardo_Guimar%C3%A3es",
+tags: "Bernardo Guimarães Romantismo Literatura Brasileira Romance Regionalista O Guarani Índio Natureza"
+},
+   
+{
+titulo: "Camilo Castelo Branco",
+descricao: "Camilo Castelo Branco foi um romancista português, mas sua obra teve grande influência na literatura brasileira. Seus romances, marcados pelo sentimentalismo e pelo melodrama, abordam temas como o amor, a paixão e a traição",
+Link: "https://pt.wikipedia.org/wiki/Camilo_Castelo_Branco",
+tags: "Camilo Castelo Branco Romance Literatura Portuguesa Sentimentalismo Melodrama Amor Paixão Traição"
+},
+
+{
+titulo: "Casimiro de Abreu",
+descricao: "Casimiro de Abreu foi um poeta romântico brasileiro, conhecido por seus poemas que expressam saudosismo, melancolia e um profundo amor pela natureza.",
+Link: "https://pt.wikipedia.org/wiki/Casimiro_de_Abreu",
+tags: "Casimiro de Abreu Romantismo Poesia Brasileira Saudosismo Melancolia Natureza Infância"
+},
+]
